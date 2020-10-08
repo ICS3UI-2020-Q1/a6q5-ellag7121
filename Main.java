d8e641382dd775c303cd3b237697edc2ca6084db
ellag7121 <ellag7121@wrdsb.ca>
2020-10-08 14:59:58 +0000

    finished a6q5

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,35 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner to get the user's input
+    Scanner input = new Scanner(System.in);
+
+    //initialize variables
+    int temp; //the temporary variable to store the number in
     
+    //create the array
+    int[] numbers = new int[2];
+
+    //ask the user for the numbers
+    System.out.println("Please enter the 2 values for the array");
+    for(int i = 0; i < numbers.length; i++){
+      //store the user's numbers in the array
+      numbers[i] = input.nextInt();
+    }
+
+    //if the first number is bigger
+    if (numbers[0] > numbers[1]){
+      //store the first number in the temporary variable
+      temp = numbers[0];
+      //move the second number to the first index
+      numbers[0] = numbers[1];
+      //set the second number to the temporary variable (which holds the first number)
+      numbers[1] = temp;
+    }
+
+    //tell the user the numbers
+    System.out.println("Your numbers in ascending order are "+ numbers[0] + " and " + numbers[1]);
+
+
   }
 }
